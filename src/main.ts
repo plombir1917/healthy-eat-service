4862f99b2afe0f8d175863a68973ce96296e9e53
plombir1917 <dfhhggh@list.ru>
2025-05-01 13:41:06 +0300

    feat(main.ts): global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('healthy-eat');
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
